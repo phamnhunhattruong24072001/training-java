e9003ef7f911b127219de4a2be7a114c20b890d8
truongpnn <truongpnn.nta@gmail.com>
2025-03-21 15:43:30 +0700

    [FEATURE]: build source 1

---
+++ src/main/java/com/mycompany/myapp/controller/CustomerController.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CustomerController {
+
+    @GetMapping("/api")
+    public ResponseEntity<?> getJson() {
+        return ResponseEntity.ok(Map.of("message", "Hello World", "status", "success"));
+    }
+}
